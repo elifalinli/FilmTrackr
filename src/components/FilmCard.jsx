3f16fcbf4ac3a5d1b2d23d6ed01dca908f57bca3
Elif Alinli <semraelifalinli@gmail.com>
2024-02-18 17:07:28 +0000

    add props?

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import { ScrollArea } from "./ui/scroll-area";
 
 
-const FilmCard = ({film, id}) => {
-// console.log(film)
+const FilmCard = ({film}) => {
   
   return (
     <ScrollArea className="h-full rounded-md">
       <div className="flex items-center justify-center h-full ">
         <div className="mx-auto rounded-xl overflow-hidden shadow-sm bg-slate-100 h-60">
                 <img
-                  key={id}
-                  src={`http://image.tmdb.org/t/p/w500/${film.poster_path}`}
+                  key={film.props?.id}
+                  src={`http://image.tmdb.org/t/p/w500/${film.props?.poster_path}`}
                   className="w-full h-full rounded-xl object-cover"
-                  alt={film.original_title}
+                  alt={film.props?.original_title}
                 />
        
         </div>

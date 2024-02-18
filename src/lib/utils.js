9e7c97558e9b566f08079a35cb12d300e650b56f
Elif Alinli <semraelifalinli@gmail.com>
2024-02-18 17:08:57 +0000

    add options

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -10,10 +10,21 @@ export function cn(...inputs) {
 }
 
 const getCardsByCategory = async (api) => {
-  const res = await fetch(api);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OWE0YTVhNDRjZTFjOTAyNTg1MmRjNmQzYmUxOGQ5MyIsInN1YiI6IjY1Y2ExNjZkY2U2YzRjMDE3Y2JiZjI5YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sIQooiDRT5Uh_bXctDj400sO2nKGRJy7qB9xIsytqM0",
+    },
+  };
+
+  const res = await fetch(api, options);
   const data = await res.json();
 
-  const cardComponents = data.results?.map((movie, i) => <FilmCard key={i} {...movie} />);
+  const cardComponents = data.results?.map((movie, i) => {
+    return <FilmCard key={i} {...movie} />;
+  });
 
   return cardComponents;
 };

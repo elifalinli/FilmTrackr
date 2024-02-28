98c70266bcf50190182e274a7b22020508053dfe
Elif Alinli <semraelifalinli@gmail.com>
2024-02-28 13:29:24 +0000

    delete unnecessary code

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,8 +2,6 @@ import FilmCard from "@/components/FilmCard";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-const apiKEY = process.env.API_KEY;
-const apiToken = process.env.API_TOKEN;
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
@@ -23,6 +21,7 @@ const getCardsByCategory = async (api) => {
   const data = await res.json();
 
   const cardComponents = data.results?.map((movie, i) => {
+  
     return <FilmCard key={i} {...movie} />;
   });
 
